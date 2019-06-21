95cc66bdd6930ed7a9554594bc737db3703b5d50
nico <brun.nicolas21@gmail.com>
2019-06-21 20:55:33 +0200

    print list

--- cmd/scan/main.go
+++ cmd/scan/main.go
@@ -7,9 +7,11 @@ import (
 	"os"
 
 	"github.com/busoc/rt"
+	"github.com/midbel/xxh"
 )
 
 func main() {
+	list := flag.Bool("l", false, "list")
 	flag.Parse()
 
 	mr, err := rt.Browse(flag.Args(), true)
@@ -22,13 +24,17 @@ func main() {
 	var (
 		size int
 		bad  int
-		rs   = rt.NewReader(mr)
+		sum  = xxh.New64(0)
 		buf  = make([]byte, 8<<20)
+		rs   = io.TeeReader(rt.NewReader(mr), sum)
 	)
 	for i := 1; ; i++ {
 		n, err := rs.Read(buf)
 		switch err {
 		case nil:
+			if *list {
+				fmt.Printf("%7d | %7d | %016x\n", i, n, sum.Sum64())
+			}
 			size += n - 4
 		case io.EOF, rt.ErrInvalid:
 			fmt.Printf("%d packets (%d invalid, %dKB)\n", i-1, bad, size>>10)
